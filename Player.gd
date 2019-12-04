cff96e4c5c907e1afe253320a77110fc162be402
sztxr <sztxr@github.com>
2019-12-04 20:10:12 -0300

    initial commit

---
+++ Player.gd
@@ -0,0 +1,38 @@
+extends Area2D
+
+export var speed = 400 # how fast the player will move (pixels/sec)
+var screen_size # size of the game window
+
+# called when a node enters the scene tree
+func _ready():
+    screen_size = get_viewport_rect().size
+    # hide()
+
+# called every frame
+func _process(delta):
+    var velocity = Vector2() # the player's movement vector
+    if Input.is_action_pressed("ui_right"):
+        velocity.x += 1
+    if Input.is_action_pressed("ui_left"):
+        velocity.x -= 1
+    if Input.is_action_pressed("ui_down"):
+        velocity.y += 1
+    if Input.is_action_pressed("ui_up"):
+        velocity.y -= 1
+    if velocity.length() > 0:
+        velocity = velocity.normalized() * speed
+        $AnimatedSprite.play()
+    else:
+        $AnimatedSprite.stop()
+    # prevent player from leaving the screen
+    position += velocity * delta
+    position.x = clamp(position.x, 0, screen_size.x)
+    position.y = clamp(position.y, 0, screen_size.y)
+    # choose animation
+    if velocity.x != 0:
+        $AnimatedSprite.animation = "right"
+        $AnimatedSprite.flip_v = false
+        $AnimatedSprite.flip_h = velocity.x < 0
+    elif velocity.y != 0:
+        $AnimatedSprite.animation = "up"
+        $AnimatedSprite.flip_v = velocity.y > 0
